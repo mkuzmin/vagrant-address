e45a3b8b7231d394a4cb4f0413b0f6ffc7cbe073
Michael Kuzmin <mkuzmin@gmail.com>
2015-01-06 00:05:33 +0300

    bundle gem vagrant-guestip

---
+++ lib/vagrant/guestip.rb
@@ -0,0 +1,7 @@
+require "vagrant/guestip/version"
+
+module Vagrant
+  module Guestip
+    # Your code goes here...
+  end
+end
---
+++ lib/vagrant/guestip/version.rb
@@ -0,0 +1,5 @@
+module Vagrant
+  module Guestip
+    VERSION = "0.0.1"
+  end
+end
