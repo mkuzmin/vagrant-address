9c2f68e18e8a70172b49bbc022de6e8ea9862d6a
Michael Kuzmin <michael.kuzmin@jetbrains.com>
2015-04-28 13:48:24 +0300

    Access IP address using VM provider API, the same way as `vagrant ssh` does.
    Calling a code within guest VM can give different results, i.e. issues with `docker0` interface registered as a first network adapter.

--- lib/command.rb
+++ lib/command.rb
@@ -7,7 +7,7 @@ module VagrantPlugins
 
       def execute
         with_target_vms(nil, {:single_target=>true}) do |machine|
-          ip = machine.guest.capability(:read_ip_address)
+          ip = machine.provider.capability(:public_address)
           message = ENV['TEAMCITY_VERSION'] ? "##teamcity[setParameter name='env.VAGRANT_GUESTIP' value='#{ip}']" : ip
           @env.ui.info(message)
         end
